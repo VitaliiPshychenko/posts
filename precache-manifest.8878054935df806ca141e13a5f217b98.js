fb08b22bd58fe2658353b4a128efe7ed292f154a
Vitalii Pshychenko <pshychenko94@gmail.com>
2019-07-23 09:29:12 +0300

    Updates

---
+++ precache-manifest.8878054935df806ca141e13a5f217b98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb532bfd3ad6e058d02bb15086cfe81",
+    "url": "/posts/index.html"
+  },
+  {
+    "revision": "5d48fc28d7b7082330f1",
+    "url": "/posts/static/css/main.8a21b779.chunk.css"
+  },
+  {
+    "revision": "6ca1de3480c73b42cd18",
+    "url": "/posts/static/js/2.7b40619e.chunk.js"
+  },
+  {
+    "revision": "5d48fc28d7b7082330f1",
+    "url": "/posts/static/js/main.cc4ab50e.chunk.js"
+  },
+  {
+    "revision": "109590637415ee219c3d",
+    "url": "/posts/static/js/runtime~main.a4b2063c.js"
+  }
+]);
